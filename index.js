3560182961b0080bda8b7e9abbdb89f276a0f1e3
Joshua Suskin <jsuskin85@gmail.com>
2019-04-05 06:32:33 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 // const deliLine = [];
 
 function takeANumber(cur, name) {
-  return `Welcome, ${name}.  You are number ${cur + 1}`;
+  return `Welcome, ${name}. You are number ${cur.length + 1} in line.`;
+}
+
+function nowServing(katzDeliLine) {
+  return katzDeliLine.length > 0 ? katzDeliLine.splice(0, 1) : "";
 }
