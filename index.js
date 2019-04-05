977589b298c0862ac5cc373b3e19a296ba7389d0
Joshua Suskin <jsuskin85@gmail.com>
2019-04-05 06:47:40 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -1,11 +1,9 @@
-// const deliLine = [];
-
 function takeANumber(cur, name) {
   return `Welcome, ${name}. You are number ${cur.length + 1} in line.`;
 }
 
-function nowServing(deliLine) {
-  return deliLine.length > 0 ? deliLine.splice(0, 1) : "There is nobody waiting to be served!";
+function nowServing(cur) {
+  return cur.length > 0 ? cur.splice(0, 1) : "There is nobody waiting to be served!";
 }
 
 function currentLine(cur) {
